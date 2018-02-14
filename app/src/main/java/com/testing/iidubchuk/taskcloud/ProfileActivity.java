bb563d47420aa1178673dd07024205d1e829524f
iidubchuk <iidubchuk@gmail.com>
2018-02-14 13:41:21 +0200

    "ProfileActivity" add btn_exit

--- app/src/main/java/com/testing/iidubchuk/taskcloud/ProfileActivity.java
+++ app/src/main/java/com/testing/iidubchuk/taskcloud/ProfileActivity.java
@@ -1,19 +1,62 @@
 package com.testing.iidubchuk.taskcloud;
 
 import android.content.Intent;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ProfileActivity extends AppCompatActivity {
-
+    private TextView name;
+    private TextView mail;
+    private FirebaseUser fuser;
+    private DatabaseReference mRef;
+    private  User user;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        name = findViewById(R.id.Profile_tv_name);
+        mail = findViewById(R.id.Profile_tv_mail);
+
+        fuser = FirebaseAuth.getInstance().getCurrentUser();
+        mRef = FirebaseDatabase.getInstance().getReference("/users/"+fuser.getUid());
+
+        mRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                user = dataSnapshot.getValue(User.class);
+                UpdeteUI();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
+
     }
+    private void UpdeteUI()
+    {
+        if (user!=null)
+        {
+            name.setText("Name: "+user.Name);
+            mail.setText("Mail: "+user.Email);
+        }
+    }
+
 
     public void onClick(View view) {
         switch (view.getId())
